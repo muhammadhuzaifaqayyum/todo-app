5d8882a43ab2bc2478f3b63025060ec35e8353ea
huzaifa <connectalhuzaifiii@gmail.com>
2023-06-12 15:00:27 +0500

    completed

--- src/todolist/TodoItem.jsx
+++ src/todolist/TodoItem.jsx
@@ -2,12 +2,19 @@
 import DeleteIcon from "../assets/deleteicon.png"
 import TikIcon from "../assets/tik.png"
 import "./todo.css"
+import { useState } from "react"
 
-export const TodoItem = ({ title, deleteTodo }) => {
+
+export const TodoItem = ({ title, deleteTodo, completeTodo }) => {
+
+const [color,setColor]=useState(false);
+const doneTodo=()=>{
+setColor(!color)
+}
   return (
     <div className="todo-item" id="todo-1">
-      <p className="todo-task-title">{title}</p>
-      <img className="todo-tik-icon" src={TikIcon}  />
+      <p className={color ? "checked":"todo-task-title" } onClick={doneTodo} >{title}</p>
+      <img className="todo-tik-icon" src={TikIcon} onClick={doneTodo} />
       <img className="todo-icon" src={DeleteIcon} onClick={deleteTodo} />
     </div>
   )
